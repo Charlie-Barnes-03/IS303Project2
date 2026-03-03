c0427e9871014448bc4f1039a5a07f06e9fa3142
Charlie-Barnes-03 <charbar4@byu.edu>
2026-03-03 13:45:03 -0700

    Create intro function

---
+++ soccerSimulation.py
@@ -0,0 +1,8 @@
+# Charlie Barnes, 
+# P2 Soccer Simulation
+
+def intro_display(playerName) :
+    playerName = input("Enter your name: ")
+    print(f"\nWelcome {playerName}! This program will simulate a soccer season\
+          where you provide the team names and the scores are randomly generated!")
+    return playerName
