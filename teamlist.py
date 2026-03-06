bc236be2d316e65516a646e8fa9f1692d9201ad5
robsvey <robbiesoccer12@gmail.com>
2026-03-06 16:35:05 -0700

    Update teamlist.py
    Added the selection process to be input into the game function

--- teamlist.py
+++ teamlist.py
@@ -1,5 +1,5 @@
 #Robbie McVey
-#Pre Built List of Teams
+#Pre Built List of Teams and select teams
 lstTeams = ["North Carolina Tar Heels",
     "Stanford Cardinal",
     "UCLA Bruins",
@@ -11,3 +11,22 @@ lstTeams = ["North Carolina Tar Heels",
     "Texas Longhorns",
     "Portland Pilots",
     "BYU Cougars"]
+
+def chooseteam(sExcludedTeam =''):
+    iTeamNum = 0
+    print("Here is the list of teams available to choose.")
+    for t in lstTeams:
+        if t != sExcludedTeam:
+            print(f'{iTeamNum + 1}. {t}')
+            iTeamNum += 1
+    iChoice = int(input("Please choose a team from the list: "))
+    sSelectedTeam = lstTeams[iChoice - 1]
+    lstTeams.remove(sSelectedTeam)
+    return sSelectedTeam
+print('\n Choose a Home Team.')
+sHomeTeam = chooseteam()
+print('\n Choose an Away Team')
+sAwayTeam = chooseteam(sHomeTeam)
+
+print(f'Home team: {sHomeTeam}')
+print(f'Away team: {sAwayTeam}')
