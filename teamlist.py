1bdbf3190b61f6d155389193c486fcd4197764b2
robsvey <robbiesoccer12@gmail.com>
2026-03-06 12:27:53 -0700

    teamlist.py
    prebuilt list for game

---
+++ teamlist.py
@@ -0,0 +1,13 @@
+#Robbie McVey
+#Pre Built List of Teams
+lstTeams = ["North Carolina Tar Heels",
+    "Stanford Cardinal",
+    "UCLA Bruins",
+    "Florida State Seminoles",
+    "Penn State Nittany Lions",
+    "Georgetown Hoyas",
+    "Virginia Cavaliers",
+    "Notre Dame Fighting Irish",
+    "Texas Longhorns",
+    "Portland Pilots",
+    "BYU Cougars"]
