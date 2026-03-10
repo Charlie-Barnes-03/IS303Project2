23db10cd2d2e6f7666d454b45c7dc871bfed3889
Eli Weaver <jeweav04@byu.edu>
2026-03-09 23:30:15 -0600

    fully functional game i think

--- functional_game.py
+++ functional_game.py
@@ -1,5 +1,8 @@
 #Robbie McVey, Charlie Barns, Eli Weaver
 
+#import random for generating scores of games
+import random
+
 #function to display name entry and explantation of game
 def intro_display() :
     playerName = input("Enter your name: ")
@@ -20,18 +23,48 @@ def chooseteam(sExcludedTeam =''):
 
 
 def showMenu() :
-    while (True) :
+    bPlay = True
+
+    #Establish variables to keep track of the total wins and losses.
+    iHomeWins = 0
+    iHomeLosses = 0
+
+    while (bPlay == True) :
         print(" 1 - Play the game")
         print(" 2 - Display final record")
         print(" 3 - Exit\n")
 
         iChoice = int( input("Make a selection: "))
 
-        if (iChoice != 1 and iChoice != 2 and iChoice != 3) :
-            print("Enter a valid choice!")
-        else :
-            return iChoice
+        
+        if iChoice == 1 :
+            # A function that creates random numbers which represent the score of soccer games
+            def scores() :
+                global iHomeScore, iAwayScore
+                iHomeScore = random.randrange(0,4)
+                iAwayScore = random.randrange(0,4)
+                return iHomeScore, iAwayScore
 
+
+            # generate scores for the game, make sure there is no tie
+            iHomeScore, iAwayScore = scores()
+            while iHomeScore == iAwayScore :
+                iHomeScore, iAwayScore = scores()
+
+            if iHomeScore > iAwayScore :
+                iHomeWins = iHomeWins + 1
+                print(f"\t{sHomeTeam}'s score: {iHomeScore} - {sAwayTeam}'s score: {iAwayScore}\n")
+
+            elif iHomeScore < iAwayScore :
+                iHomeLosses = iHomeLosses + 1
+                print(f"\t{sHomeTeam}'s score: {iHomeScore} - {sAwayTeam}'s score: {iAwayScore}\n")
+
+        if iChoice == 2 :
+            print(f"{sHomeTeam} has a final record of {iHomeWins} wins and {iHomeLosses} losses.\n")
+
+        if iChoice == 3 :
+            print(f"Thanks for playing {sPlayerName}! Come back soon.\n")
+            bPlay = False
 
 sPlayerName = intro_display()
 
