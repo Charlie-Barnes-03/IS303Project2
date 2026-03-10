dd2e3c25a70237b3ba3980c48260fc26abce57a8
Charlie-Barnes-03 <charbar4@byu.edu>
2026-03-09 22:34:26 -0600

    Update soccerSimulation.py

--- soccerSimulation.py
+++ soccerSimulation.py
@@ -1,8 +1,11 @@
 # Charlie Barnes, 
 # P2 Soccer Simulation
 
-def intro_display(playerName) :
+def intro_display() :
     playerName = input("Enter your name: ")
-    print(f"\nWelcome {playerName}! This program will simulate a soccer season\
-          where you provide the team names and the scores are randomly generated!")
+    print(f"\nWelcome {playerName}! This program will simulate a soccer season where you provide the team names and the scores are randomly generated!")
     return playerName
+
+sPlayerName = intro_display()
+
+print(sPlayerName)
